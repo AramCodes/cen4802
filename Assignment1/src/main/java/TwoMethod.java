0081ff9327b3b89a8ea84a8d34c1490627feab11
Aram <115132116+AramCodes@users.noreply.github.com>
2026-01-13 16:47:24 -0500

    fibRecursive added to main branch
    classname is TwoMethod.java

---
+++ Assignment1/src/main/java/TwoMethod.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoMethod {
+
+    public static int fibRecursive(int n) {
+        List<Integer> fibonacciSeq = new ArrayList<>();
+        fibonacciSeq.add(0);
+        fibonacciSeq.add(1);
+
+        for(int i = 2; i <= n; i++){
+            fibonacciSeq.add(
+                    fibonacciSeq.get(i - 1) + fibonacciSeq.get(i - 2)
+            );
+        }
+
+        return fibonacciSeq.get(n);
+    }
+    public static void main(String[] args){
+        System.out.println("");
+        System.out.println("The nth term of the Fibonacci sequence is " + fibRecursive(10));
+    }
+
+
+}
